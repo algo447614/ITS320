8d41462503a46b8ee51b5fe634ee1e09a1ce1e51
algo447614 <147664969+algo447614@users.noreply.github.com>
2024-06-26 08:55:35 -0400

    Create CTA5.py

---
+++ Portfolio/CTA5.py
@@ -0,0 +1,33 @@
+#------------------------------------------------------------------------
+# Program Name: ITS320_CTA5_Option2.py
+# Author: Aliciana Gondick
+# Date: Sept. 17, 2023
+# Program Objective: Allow for user to input three strings...
+# Concatenate the first two, and reverse the third.
+#------------------------------------------------------------------------
+# Pseudocode:
+#   1. Program will prompt for three strings to be input by user.
+#   2. Function will concatenate the first two strings, and reverse the third
+#   3. Function will return concatenation, and reversed string
+#------------------------------------------------------------------------
+# Program Inputs: Three strings
+# Program Outputs: Concatenation of two strings, reversed third string
+#------------------------------------------------------------------------
+
+
+
+print("This program will concatenate two strings, and reverse the third.")
+
+
+
+def StringFunction():
+    String1 = input("type a word or phrase: ")
+    String2 = input("type another word or phrase: ")
+    String3 = input("this will be reversed: ")
+    a = String1 + String2 + ", "
+    b = String3[::-1]
+    c = a + b
+    return c
+print(StringFunction())
+    
+print("Thank you for using this program.")
